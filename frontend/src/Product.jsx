3953a1325d8992e6ec03d6bb7e9d5c18e8910018
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-10-26 13:20:30 +0300

    Refactored..

--- frontend/src/Product.jsx
+++ frontend/src/Product.jsx
@@ -1,44 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import { Button, Table } from "reactstrap";
 
-export default class Product extends Component {
-  render() {
-    return (
-      <div>
-        <h3>{this.props.info.title}</h3>
-        <h4>{this.props.info.currentCategory}</h4>
-        <Table striped>
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Product Name</th>
-              <th>Quantity Per Unit</th>
-              <th>Unit Price</th>
-              <th>Units In Stock</th>
-              <th></th>
+const Product = (props) => {
+  const info = props.info;
+  return (
+    <div>
+      <h3>{info.title}</h3>
+      <h4>{info.currentCategory}</h4>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Product Name</th>
+            <th>Quantity Per Unit</th>
+            <th>Unit Price</th>
+            <th>Units In Stock</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {info.products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.id}</td>
+              <td>{product.productName}</td>
+              <td>{product.quantityPerUnit}</td>
+              <td>{product.unitPrice}</td>
+              <td>{product.unitsInStock}</td>
+              <td>
+                <Button onClick={() => info.addToCart(product)} color="info">
+                  Add
+                </Button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {this.props.info.products.map((product) => (
-              <tr key={product.id}>
-                <td>{product.id}</td>
-                <td>{product.productName}</td>
-                <td>{product.quantityPerUnit}</td>
-                <td>{product.unitPrice}</td>
-                <td>{product.unitsInStock}</td>
-                <td>
-                  <Button
-                    onClick={() => this.props.info.addToCart(product)}
-                    color="info"
-                  >
-                    Add
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Product;
